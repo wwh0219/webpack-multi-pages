b5ae7745a8034900e7c8420d9de933833895162a
gaarashura <gaarashura0@gmail.com>
2018-05-13 22:39:36 +0800

    😃

--- config/paths.js
+++ config/paths.js
@@ -4,7 +4,7 @@ const internalIp = require('internal-ip');
 const host='http://'+internalIp.v4.sync()
 const utils=require('../build/utils')
 //扫描每个页面入口js文件
-const entryArray=glob.sync(path.resolve(__dirname,'../src/**/scripts'))
+const entryArray=glob.sync(path.resolve(__dirname,'../src/pages/**/scripts'))
 
 
 const entry={};//wenpack entryMap
